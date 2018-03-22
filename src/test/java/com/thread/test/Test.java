6588feb1ccd40faad8d0ce18ebc7b74735d9ae11
xiaxb <xiaxb@shicaidai.com>
2018-03-22 12:07:28 +0800

    添加测试类

---
+++ src/test/java/com/thread/test/Test.java
@@ -0,0 +1,32 @@
+package com.thread.test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.service.impl.DataServiceImpl;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/applicationContext.xml")
+public class Test {
+
+    @Autowired
+    private DataServiceImpl dataService;
+    
+	public static void main(String[] args) {
+//		String a = "121212";
+//		if(true){
+//			String b=a;
+//			System.out.println(b);
+//		}
+	}
+	
+	@org.junit.Test
+	public void testDataService() {
+	    String a = "121212";
+	    if(true){
+          String b=a;
+          System.out.println(b);
+      }
+	}
+
+}
