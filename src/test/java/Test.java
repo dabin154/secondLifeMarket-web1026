1fb889f37a7e2c9f4adb5a4637e674c338ddb59b
xiaxb <xiaxb@shicaidai.com>
2018-04-13 20:50:45 +0800

    多线程测试案例

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,47 @@
+import com.thread.test.NewThreadExtendsThread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Test {
 
 	public static void main(String[] args) {
-		String a = "121212";
-		if(true){
-			String b=a;
-			System.out.println(b);
+
+//		Thread thread1 = new Thread(new NewThreadExtendsThread("dabin1"));
+//		Thread thread2 = new Thread(new NewThreadExtendsThread("dabin2"));
+//		Thread thread3 = new Thread(new NewThreadExtendsThread("dabin3"));
+//		thread1.start();
+//		thread2.start();
+//		thread3.start();
+//		try {
+//			thread1.join();
+//			thread2.join();
+//			thread3.join();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+
+		ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+
+		for (int i = 1; i <= 10; i++) {
+			final int index = i;
+			fixedThreadPool.execute(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						String threadName = Thread.currentThread().getName();
+						System.out.println("执行：" + index + "，线程名称：" + threadName);
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+
+						e.printStackTrace();
+					}
+				}
+			});
 		}
 
 	}
+
 
 }
--- src/test/java/com/thread/test/NewThreadExtendsThread.java
+++ src/test/java/com/thread/test/NewThreadExtendsThread.java
@@ -1,18 +1,24 @@
 package com.thread.test;
 
-public class NewThreadExtendsThread extends Thread{
-    
-    private String name;                 
-    
+public class NewThreadExtendsThread extends Thread {
+
+    private String name;
+
     public NewThreadExtendsThread(String name) {
         this.name = name;
     }
-    
+
     @Override
     public void run() {
-        super.run();
-    }
-    
-    
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s is running %d\n", name, i);
 
+        }
+    }
 }
+
